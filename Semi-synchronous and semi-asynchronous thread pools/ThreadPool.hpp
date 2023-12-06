55369db9395c6c380c09c2216667aeb47ba03963
arielsone <9355823+arielsone@user.noreply.gitee.com>
2023-12-06 02:47:52 +0000

    线程池
    Signed-off-by: arielsone <9355823+arielsone@user.noreply.gitee.com>

---
+++ Semi-synchronous and semi-asynchronous thread pools/ThreadPool.hpp
@@ -0,0 +1,86 @@
+#include<list>
+#include<thread>
+#include<functional>
+#include<memory>
+#include <atomic>
+#include "SyncQueue.hpp"
+using namespace std;
+
+const int MaxTaskCount = 100;
+
+class ThreadPool
+{
+public:
+    using Task = std::function<void()>;
+    ThreadPool(int numThreads = std::thread::hardware_concurrency()):m_queue(MaxTaskCount)
+    {
+        //开启线程池
+        Start(numThreads);
+    }
+    ~ThreadPool()
+    {
+        //如果没有停止则主动停止线程池
+        Stop();
+    }
+
+    void Stop()
+    {
+        //保证多线程情况下只调用一次StopThreadGroup
+        std::call_once(m_flag,[this](){StopThreadGroup();});
+    }
+
+    void AddTask(Task &&task)
+    {
+        m_queue.Put(std::forward<Task>(task));
+    }
+
+    void AddTask(Task &task)
+    {
+        m_queue.Put(task);
+    }
+private:
+    void Start(int numThreads)
+    {
+        m_running = true;
+        //创建线程数组
+        for(int i = 0;i < numThreads; i++)
+        {
+            m_threadgroup.push_back(std::make_shared<std::thread>(&ThreadPool::RunInThread, this));
+        }
+    }
+
+    void RunInThread()
+    {
+        while(m_running)
+        {
+            //取任务分别执行
+            std::list<Task> list;
+            m_queue.Take(list);
+            for(auto& task : list)
+            {
+                if(!m_running)
+                    return;
+                task();
+            }
+        }
+    }
+
+    void StopThreadGroup()
+    {
+        m_queue.Stop();         //让同步队列中的线程停止
+        m_running = false;      //置为false，让内部线程跳出循环并退出
+        for(auto thread : m_threadgroup)
+        {
+            if(thread->joinable())
+                thread->join();
+        }
+
+        m_threadgroup.clear();
+
+    }
+private:
+    std::list<std::shared_ptr<std::thread>> m_threadgroup;      //处理任务的线程组
+    SyncQueue<Task> m_queue;                                    //同步队列
+    atomic_bool m_running;                                      //是否停止标识
+    std::once_flag m_flag;                                      
+};
